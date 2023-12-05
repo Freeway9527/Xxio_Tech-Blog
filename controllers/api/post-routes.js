4068de5f761cce3d467943d92da96aeb592ec652
xiongxeng <xiongxeng@gmail.com>
2023-12-05 14:27:39 -0600

    add api routes for comment and post

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,126 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      order: [["created_at", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
+      ]
+    });
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Get post with ID associated with user
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: { id: req.params.id },
+      attributes: ["id", "content", "title", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        }
+      ]
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Create a post
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Update a post
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+
+    if (!postData[0]) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    res.json(postData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
