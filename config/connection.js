78749b6322810e446db02de8e292882c361593db
xiongxeng <xiongxeng@gmail.com>
2023-12-10 21:43:59 -0600

    debugging port to see if can upload on render

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,15 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // For local development or backup configuration
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-     host: 'localhost',
+      host: process.env.DB_HOST, 
       dialect: 'mysql',
-      port: 3306
+      port: process.env.DB_PORT, 
     }
   );
 }
