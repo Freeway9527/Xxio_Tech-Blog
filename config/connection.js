7f963de584743636fd327378450294b84afd11d5
xiongxeng <xiongxeng@gmail.com>
2023-12-13 19:12:41 -0600

    debug render hosting issue

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,28 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
 if (process.env.POSTGRES_DB) {
-  sequelize = new Sequelize(process.env.POSTGRES_DB);
+  sequelize = new Sequelize(
+    process.env.POSTGRES_DB, 
+    process.env.POSTGRES_USER, 
+    process.env.POSTGRES_PASSWORD, 
+    {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    dialect: "postgres",
+    port: process.env.POSTGRES_PORT || 5432,
+  });
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
     {
-     host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+    host: process.env.DB_HOST || 'localhost',
+    dialect: "mysql",
+    port: process.env.DB_PORT || 3306,
+  });
 }
 
 module.exports = sequelize;
